317c52227046974256fb47b116371ff1b9e190fc
rsankutty <r.sankutty@gmail.com>
2023-01-14 19:59:55 -0300

    rota POST participants sem validacao

--- src/app.js
+++ src/app.js
@@ -28,6 +28,29 @@ try {
 	console.log(err.message);
 }
 
+// Rota POST participants
+server.post('/participants', async (req, res) => {
+	const { name } = req.body;
+
+    const registeredUser = await db.collection('participants').findOne({ name });
+    if (registeredUser) return res.status(409).send('Username already in use');
+
+    // Saving user in database
+    await db.collection("participants").insertOne({name: name, lastStatus: Date.now()})
+
+    // Saving entering message in database
+	await db.collection('messages').insertOne({
+		from: name,
+		to: 'Todos',
+		text: 'entra na sala...',
+		type: 'status',
+		time: dayjs(Date.now()).format('hh:mm:ss'),
+	});
+
+    res.status(201).send('User successfully logged in ');
+});
+
+
 server.get('/participants', async (req, res) => {
 	const users = await db.collection('participants').find().toArray();
 	res.send(users);
