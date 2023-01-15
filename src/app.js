5ef6f8ae383c2d97c648ec3b5bcb2777787b8a06
rsankutty <r.sankutty@gmail.com>
2023-01-15 02:09:36 -0300

    GET messages sem validacao

--- src/app.js
+++ src/app.js
@@ -21,57 +21,78 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 try {
-	await mongoClient.connect();
-	db = mongoClient.db();  
-    console.log("Successfully connected to the database"); 
+    await mongoClient.connect();
+    db = mongoClient.db();
+    console.log("Successfully connected to the database");
 } catch (err) {
-	console.log(err.message);
+    console.log(err.message);
 }
 
 // Rota POST participants
 server.post('/participants', async (req, res) => {
-	const { name } = req.body;
+    const { name } = req.body;
 
     const registeredUser = await db.collection('participants').findOne({ name });
     if (registeredUser) return res.status(409).send('Username already in use');
 
     // Saving user in database
-    await db.collection("participants").insertOne({name: name, lastStatus: Date.now()})
+    await db.collection("participants").insertOne({ name: name, lastStatus: Date.now() })
 
     // Saving entering message in database
-	await db.collection('messages').insertOne({
-		from: name,
-		to: 'Todos',
-		text: 'entra na sala...',
-		type: 'status',
-		time: dayjs(Date.now()).format('hh:mm:ss'),
-	});
+    await db.collection('messages').insertOne({
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs(Date.now()).format('hh:mm:ss'),
+    });
 
-    res.status(201).send('User successfully logged in ');
+    res.status(201).send('User successfully logged in');
 });
 
 // Rota GET participants
 server.get('/participants', async (req, res) => {
-	const users = await db.collection('participants').find().toArray();
-	res.send(users);
+    const users = await db.collection('participants').find().toArray();
+    res.send(users);
 });
 
 
 // Rota POST messages
 server.post('/messages', async (req, res) => {
-	const { to, text, type } = req.body;
+    const { to, text, type } = req.body;
     const from = req.headers.user;
 
     const loggedUser = await db.collection('participants').findOne({ from });
     if (loggedUser) return res.status(422).send('Unregistered user');
 
-    await db.collection('messages').insertOne({
-		from,
-		to,
-		text,
-		type,
-		time: dayjs(Date.now()).format('hh:mm:ss'),
-	});
+    await db.collection("messages").insertOne({
+        from,
+        to,
+        text,
+        type,
+        time: dayjs(Date.now()).format('hh:mm:ss'),
+    });
 
-    res.status(201).send('Message posted successfully');
+    res.status(201).send("Message posted successfully");
+});
+
+// Rota GET messages
+server.get('/messages', async (req, res) => {
+    const { limit } = req.query;
+    const { user } = req.headers;
+
+    const messages = await db.collection("messages").find({
+        $or:
+            [
+                { to: "Todos" },
+                { to: user },
+                { from: user }
+            ]
+    }).toArray();
+
+    const reverseMessages = messages.reverse();
+
+    if (limit) return res.send(reverseMessages.slice(0,limit));
+
+    return res.send(reverseMessages);
 });
